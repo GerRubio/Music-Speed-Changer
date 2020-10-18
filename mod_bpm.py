bfc75c9ee412534a520dd0e47bf0a4a219a9306b
GerRubio <rubio.delavieja@gmail.com>
2020-10-18 16:20:47 +0200

    Base project.

---
+++ mod_bpm.py
@@ -0,0 +1,24 @@
+import sys
+
+from spotify import Song
+
+
+'''Information about headers in datafile
+0: Track name
+1: Artist name
+2: Genre
+3: Beats per minute
+4: Energy
+5: Danceability
+6: Length'''
+
+if __name__ == '__main__':
+    input_file = 'top50.csv'
+    output_file = 'top50_mod.csv'
+    relative_bpm = int(sys.argv[1])  # Read keyboard input
+
+    # Load songs (from input_file)
+
+    # Change speed of all songs
+
+    # Save songs (to output_file)
---
+++ spotify.py
@@ -0,0 +1,18 @@
+class Song:
+    def __init__(self, track, artist, genre, bpm, energy, danceability, length):
+        # Your code here
+
+    def __str__(self):
+        # Your code here
+
+    def change_speed(self, relative_bpm):
+        # Your code here
+
+    @staticmethod
+    def load_songs(path):
+        # Your code here
+        return songs
+
+    @staticmethod
+    def save_songs(songs, path):
+        # Your code here
